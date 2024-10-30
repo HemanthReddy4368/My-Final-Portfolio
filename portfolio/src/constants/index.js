e2416393fb15839595d3e83b4437621c267117f1
hemanthreddy171 <gahredd2@newhaven.edu>
2024-10-30 17:27:52 -0400

    Changed the description

--- portfolio/src/constants/index.js
+++ portfolio/src/constants/index.js
@@ -3,39 +3,31 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a dedicated .NET developer with 3 years of experience in building and maintaining high-quality web applications. My expertise spans the .NET ecosystem, including ASP.NET Core, Entity Framework, and MSBI for comprehensive business intelligence solutions. Additionally, I bring proficiency in front-end development with React and TailwindCSS, enabling me to create seamless, user-centric interfaces. My objective is to blend robust backend systems with intuitive front-end experiences to drive efficient, scalable, and impactful solutions for business success.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a skilled .NET developer with a strong commitment to building dynamic and scalable web applications. With 3 years of professional experience, I have developed expertise in the .NET framework, especially ASP.NET Core, Entity Framework, and MSBI for creating powerful data-driven solutions. My background also includes front-end technologies such as React and TailwindCSS, allowing me to bridge the gap between server-side functionality and engaging user interfaces. I am driven by a desire to understand complex systems and turn them into intuitive, high-quality applications. Beyond coding, I am passionate about exploring new tech, staying active, and making meaningful contributions to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "2024-present",
+    role: "Graduate Assistant",
+    company: "University Of New Haven",
+    description: `In the Enrollment Operations department, I supported the application process by verifying documents and preparing I-20 forms for international students. Additionally, I occasionally assisted the team with writing code and developing small applications to streamline administrative tasks and improve operational efficiency.`,
+    technologies: ["python", "React.js", "c#", "Excel","ASP.NET"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "2022 - 2024",
+    role: "Full Stack Developer",
+    company: "GlobalLogic",
+    description: `I designed and developed user interfaces for web applications using ASP.NET and C#. I collaborated closely with backend developers to integrate frontend components with Microsoft SQL databases and MSBI services. Additionally, I contributed to the Android development team by designing mobile app interfaces, implementing responsive designs, and optimizing application performance to ensure a seamless user experience.`,
+    technologies: ["java", "c#", "MSBI", "Android", "SQL", "React.js", "ASP.NET","CI/CD"],
   },
   {
     year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+    role: "FreeLancer",
+    description: `As a freelancer in college, I created responsive websites for clients using HTML, CSS, and JavaScript. This role enhanced my project management skills and allowed me to apply my web development knowledge in real-world scenarios.`,
+    technologies: ["c#", "React.js", "ASP.NET", "sql"],
+  }
 ];
 
 export const PROJECTS = [
@@ -44,33 +36,26 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    technologies: ["HTML", "CSS", "React", "c#", "SQL","tailwindcss","ASP.NET"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "React", "tailwindcss"],
   },
   {
-    title: "Blogging Platform",
+    title: "MediConnect",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A platform for doctors and patients to connect, schedule appointments, and manage medical records.",
+    technologies: ["c#", "sql", "react", "tailwind", "ASP.NET"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "153 Bull hill Ln, West Haven, CT 06516",
+  phoneNo: "+1 475-308-0764",
+  email: "hemanthreddyreddybattula@gmail.com",
 };
